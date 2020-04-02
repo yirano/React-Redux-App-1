d80f6c0f3144fa06df5cb95221293e734583b862
yirano <yirano@protonmail.ch>
2020-04-02 19:39:11 -0400

    Begin styling navbar

--- for-the-bored/src/Components/Nav.jsx
+++ for-the-bored/src/Components/Nav.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>This is navbar</h1>
-    </div>
-  )
-}
+    <nav>
+      <div className="navLeft">
+        <h4>Shortly</h4>
+        <ul>
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Resources</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navRight">
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
