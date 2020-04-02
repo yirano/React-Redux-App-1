e4aff40fdc8a6fd31e722385907ef6ff8ae47063
yirano <yirano@protonmail.ch>
2020-04-02 17:49:09 -0400

    Initialize Sass

--- for-the-bored/src/App.js
+++ for-the-bored/src/App.js
@@ -5,6 +5,7 @@ import HeaderContainer from "./Containers/HeaderContainer";
 import FeaturesContainer from "./Containers/FeaturesContainer";
 import CallToActionContainer from "./Containers/CallToActionContainer";
 import Footer from "./Components/Footer";
+import './css/index.css'
 
 function App() {
   return (
