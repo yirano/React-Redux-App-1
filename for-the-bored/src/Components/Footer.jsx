86fe6d982ec09ac5869fd4c1bf6c3c31496b1b37
yirano <yirano@protonmail.ch>
2020-04-02 23:38:00 -0400

    Style footer

--- for-the-bored/src/Components/Footer.jsx
+++ for-the-bored/src/Components/Footer.jsx
@@ -7,27 +7,31 @@ const Footer = () => {
         <h3>Shortly</h3>
       </div>
       <div className="footerSecond">
-        <ul>
-          <li>Features</li>
-          <li>Link Shortening</li>
-          <li>Branded Links</li>
-          <li>Analytics</li>
-        </ul>
-
-        <ul>
-          <li>Resources</li>
-          <li>Blog</li>
-          <li>Developers</li>
-          <li>Support</li>
-        </ul>
-
-        <ul>
-          <li>Company</li>
-          <li>About</li>
-          <li>Our Team</li>
-          <li>Careers</li>
-          <li>Contact</li>
-        </ul>
+        <div>
+          <ul>
+            <li>Features</li>
+            <li>Link Shortening</li>
+            <li>Branded Links</li>
+            <li>Analytics</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Resources</li>
+            <li>Blog</li>
+            <li>Developers</li>
+            <li>Support</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Company</li>
+            <li>About</li>
+            <li>Our Team</li>
+            <li>Careers</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
       <div className="footerThird">
         <ul>
